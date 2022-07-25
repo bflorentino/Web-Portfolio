62fa7e7912abfd6f800a752f5551bdcc5a190212
Bryan <bryanmontero81@gmail.com>
2022-07-25 19:01:43 -0400

    Support for sending emails added

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,15 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import AOS from 'aos'
 import "aos/dist/aos.css";
 import Text from '../../language/Text';
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
 
+const validateContactForm = (name: string, email:string, message:string) => {
+  
+    const emailRegEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+
+    if(name === "") return "Name is required";
+    if(message === "") return "Message is required";
+    if(!emailRegEx.test(email)) return "Invalid email";
+    
+    return true
+}
 
 const Contact = () => {
+
+  const contact = useRef<HTMLFormElement | any >(null)
 
   useEffect(()=> {
     AOS.init()
     AOS.refresh()
 })
+
+  const handleSendButton = (e:any) => {
+   
+    e.preventDefault();
+    
+    const {from_name, from_email, message} = contact.current;
+    const isValid = validateContactForm(from_name.value, from_email.value, message.value)
+
+    if(isValid === true){
+
+      emailjs.sendForm(process.env.REACT_APP_SERVICEID as string, 
+                      process.env.REACT_APP_TEMPLATEID as string , 
+                      contact.current, 
+                      process.env.REACT_APP_PUBLICKEY as string)
+      .then(res => {
+        Swal.fire({
+          title: "Email Send",
+          icon: "success",
+          "text": "Email was sent successfully",
+          confirmButtonText: "Ok"
+        })
+        resetForm()
+      })
+    }else{
+      Swal.fire({
+        title: "Error sending email",
+        icon: "error",
+        "text": isValid,
+        confirmButtonText: "Ok"
+      })
+    }
+  }
+
+  const resetForm = () => {
+    const {from_name, from_email, message} = contact.current;
+    from_email.value = "";
+    from_name.value = "";
+    message.value = ""
+  }
+
   return (
     <>
     <div 
@@ -23,25 +77,26 @@ const Contact = () => {
       <h1 className='bg-primary text-white font-ptsans text-lg w-2/5'>
       <Text tid='contactMessage' />
       </h1>
-      <form action="" className='flex flex-col items-center bg-primary py-4 text-white w-2/5'>
+      <form ref={contact} action="" className='flex flex-col items-center bg-primary py-4 text-white w-2/5'>
         <input 
             type="text" 
-            name='name' 
-            placeholder='Your Name' 
+            name='from_name' 
+            placeholder='Your Name'
             className='border-2 border-button outline-none bg-none bg-primary w-full py-4 px-2 rounded-lg'  
           />
         <input 
           type="text" 
-          name='email' 
+          name='from_email' 
           placeholder='Your email'
           className='border-2 border-button outline-none bg-transparent bg-primary w-full mt-5 py-4 px-2 rounded-lg' 
         />
         <textarea 
           placeholder='Type Your Message'
+          name='message'
           className='border-2 border-button outline-none bg-none bg-primary w-full mt-5 py-4 px-2 rounded-lg resize-none'
           rows={5} 
         />
-          <button className='bg-button text-[#000] text-lg font-ptsans px-6 py-2 rounded-lg mt-4'>
+          <button className='bg-button text-[#000] text-lg font-ptsans px-6 py-2 rounded-lg mt-4' onClick={handleSendButton}>
           <Text tid='contactButton' />
           </button>
       </form>
