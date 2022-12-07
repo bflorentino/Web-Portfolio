d69d495f1084f13e43a9e5f9c878889abf0487a1
Bryan <bryanmontero81@gmail.com>
2022-12-06 20:58:32 -0400

    Cv updated

--- src/components/navigation/Nav.tsx
+++ src/components/navigation/Nav.tsx
@@ -21,7 +21,9 @@ const Nav = () => {
 
                 <ul className='hidden minitablet:flex flex-col minitablet:flex-row ml-4 minitablet:ml-0 minitablet:mt-4 text-[12px] minilaptop:text-[16px]' id='navlinks'>
                     <li className='mr-3 minilaptop:mr-8 mt-2 minitablet:mt-0'>
-                        <a href="#idAbout" className='font-merri text-primarytext hover:border-b-4 hover:border-button py-1'><Text tid='navAbout' /></a>
+                        <a href="#idAbout" className='font-merri text-primarytext hover:border-b-4 hover:border-button py-1'>
+                            <Text tid='navAbout' />
+                        </a>
                     </li>
                     <li className='mr-3 minilaptop:mr-8 mt-2 minitablet:mt-0'>
                         <a 
@@ -31,10 +33,14 @@ const Nav = () => {
                         </a>
                     </li>
                     <li className='mr-3 minilaptop:mr-8 mt-2 minitablet:mt-0'>
-                        <a href="#idProjects" className='font-merri text-primarytext hover:border-b-4 hover:border-button py-1'><Text tid='navProjects' /></a>
+                        <a href="#idProjects" className='font-merri text-primarytext hover:border-b-4 hover:border-button py-1'>
+                            <Text tid='navProjects' />
+                        </a>
                     </li>
                     <li className='mr-3 minilaptop:mr-8 mt-2 minitablet:mt-0'>
-                        <a href="#idContact" className='font-merri text-primarytext hover:border-b-4 hover:border-button py-1'><Text tid='navContact' /></a>
+                        <a href="#idContact" className='font-merri text-primarytext hover:border-b-4 hover:border-button py-1'>
+                            <Text tid='navContact' />
+                        </a>
                     </li>
                     <li className='mr-3 minilaptop:mr-8 mt-2 minitablet:mt-0'>
                         <button 
@@ -48,7 +54,12 @@ const Nav = () => {
                         </button>
                     </li>
                 </ul>
-                <div className='flex flex-col h-[40px] items-end mr-1 minitablet:hidden text-white mt-2 z-20' id='btn-menu' onClick={handleOpenNav}>
+                
+                <div 
+                    className='flex flex-col h-[40px] items-end mr-1 minitablet:hidden text-white mt-2 z-20' 
+                    id='btn-menu' 
+                    onClick={handleOpenNav}
+                >
                     <span className='w-[37px] h-[4px] mb-[5px] relative bg-button rounded' ></span>
                     <span className='w-[37px] h-[4px] mb-[5px] relative bg-button rounded'></span>
                     <span className='w-[37px] h-[4px] mb-[5px] relative bg-button rounded'> </span>
